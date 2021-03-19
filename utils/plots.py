1627aaded1814b63dd81e39be697c67a14446d9c
William Nguyen <william.nguyen@verkada.com>
2021-03-19 15:45:58 -0700

    disable seaborn and pandas

--- utils/plots.py
+++ utils/plots.py
@@ -11,8 +11,8 @@ import cv2
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import seaborn as sns
+# import pandas as pd
+# import seaborn as sns
 import torch
 import yaml
 from PIL import Image, ImageDraw, ImageFont
@@ -269,53 +269,53 @@ def plot_study_txt(path='', x=None):  # from utils.plots import *; plot_study_tx
     plt.savefig(str(Path(path).name) + '.png', dpi=300)
 
 
-def plot_labels(labels, names=(), save_dir=Path(''), loggers=None):
-    # plot dataset labels
-    print('Plotting labels... ')
-    c, b = labels[:, 0], labels[:, 1:].transpose()  # classes, boxes
-    nc = int(c.max() + 1)  # number of classes
-    colors = color_list()
-    x = pd.DataFrame(b.transpose(), columns=['x', 'y', 'width', 'height'])
+# def plot_labels(labels, names=(), save_dir=Path(''), loggers=None):
+#     # plot dataset labels
+#     print('Plotting labels... ')
+#     c, b = labels[:, 0], labels[:, 1:].transpose()  # classes, boxes
+#     nc = int(c.max() + 1)  # number of classes
+#     colors = color_list()
+#     x = pd.DataFrame(b.transpose(), columns=['x', 'y', 'width', 'height'])
 
-    # seaborn correlogram
-    sns.pairplot(x, corner=True, diag_kind='auto', kind='hist', diag_kws=dict(bins=50), plot_kws=dict(pmax=0.9))
-    plt.savefig(save_dir / 'labels_correlogram.jpg', dpi=200)
-    plt.close()
+#     # seaborn correlogram
+#     sns.pairplot(x, corner=True, diag_kind='auto', kind='hist', diag_kws=dict(bins=50), plot_kws=dict(pmax=0.9))
+#     plt.savefig(save_dir / 'labels_correlogram.jpg', dpi=200)
+#     plt.close()
 
-    # matplotlib labels
-    matplotlib.use('svg')  # faster
-    ax = plt.subplots(2, 2, figsize=(8, 8), tight_layout=True)[1].ravel()
-    ax[0].hist(c, bins=np.linspace(0, nc, nc + 1) - 0.5, rwidth=0.8)
-    ax[0].set_ylabel('instances')
-    if 0 < len(names) < 30:
-        ax[0].set_xticks(range(len(names)))
-        ax[0].set_xticklabels(names, rotation=90, fontsize=10)
-    else:
-        ax[0].set_xlabel('classes')
-    sns.histplot(x, x='x', y='y', ax=ax[2], bins=50, pmax=0.9)
-    sns.histplot(x, x='width', y='height', ax=ax[3], bins=50, pmax=0.9)
+#     # matplotlib labels
+#     matplotlib.use('svg')  # faster
+#     ax = plt.subplots(2, 2, figsize=(8, 8), tight_layout=True)[1].ravel()
+#     ax[0].hist(c, bins=np.linspace(0, nc, nc + 1) - 0.5, rwidth=0.8)
+#     ax[0].set_ylabel('instances')
+#     if 0 < len(names) < 30:
+#         ax[0].set_xticks(range(len(names)))
+#         ax[0].set_xticklabels(names, rotation=90, fontsize=10)
+#     else:
+#         ax[0].set_xlabel('classes')
+#     sns.histplot(x, x='x', y='y', ax=ax[2], bins=50, pmax=0.9)
+#     sns.histplot(x, x='width', y='height', ax=ax[3], bins=50, pmax=0.9)
 
-    # rectangles
-    labels[:, 1:3] = 0.5  # center
-    labels[:, 1:] = xywh2xyxy(labels[:, 1:]) * 2000
-    img = Image.fromarray(np.ones((2000, 2000, 3), dtype=np.uint8) * 255)
-    for cls, *box in labels[:1000]:
-        ImageDraw.Draw(img).rectangle(box, width=1, outline=colors[int(cls) % 10])  # plot
-    ax[1].imshow(img)
-    ax[1].axis('off')
+#     # rectangles
+#     labels[:, 1:3] = 0.5  # center
+#     labels[:, 1:] = xywh2xyxy(labels[:, 1:]) * 2000
+#     img = Image.fromarray(np.ones((2000, 2000, 3), dtype=np.uint8) * 255)
+#     for cls, *box in labels[:1000]:
+#         ImageDraw.Draw(img).rectangle(box, width=1, outline=colors[int(cls) % 10])  # plot
+#     ax[1].imshow(img)
+#     ax[1].axis('off')
 
-    for a in [0, 1, 2, 3]:
-        for s in ['top', 'right', 'left', 'bottom']:
-            ax[a].spines[s].set_visible(False)
+#     for a in [0, 1, 2, 3]:
+#         for s in ['top', 'right', 'left', 'bottom']:
+#             ax[a].spines[s].set_visible(False)
 
-    plt.savefig(save_dir / 'labels.jpg', dpi=200)
-    matplotlib.use('Agg')
-    plt.close()
+#     plt.savefig(save_dir / 'labels.jpg', dpi=200)
+#     matplotlib.use('Agg')
+#     plt.close()
 
-    # loggers
-    for k, v in loggers.items() or {}:
-        if k == 'wandb' and v:
-            v.log({"Labels": [v.Image(str(x), caption=x.name) for x in save_dir.glob('*labels*.jpg')]}, commit=False)
+#     # loggers
+#     for k, v in loggers.items() or {}:
+#         if k == 'wandb' and v:
+#             v.log({"Labels": [v.Image(str(x), caption=x.name) for x in save_dir.glob('*labels*.jpg')]}, commit=False)
 
 
 def plot_evolution(yaml_file='data/hyp.finetune.yaml'):  # from utils.plots import *; plot_evolution()
